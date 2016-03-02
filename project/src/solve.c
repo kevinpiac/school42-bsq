400cf0a12a6974a99059670b5eff5c757b83885a
Huber Nicolas <nicolas@Hubers-MacBook-Pro.local>
2016-03-02 10:53:52 +0100

    main normed

--- project/src/solve.c
+++ project/src/solve.c
@@ -1,36 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   solve.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/06 23:50:52 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/03/02 10:17:49 by nhuber           ###   ########.fr       */
+/*   Created: 2016/03/02 09:48:18 by nhuber            #+#    #+#             */
+/*   Updated: 2016/03/02 10:07:24 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "header.h"
-
-int	main(int ac, char **av)
+void	multiple_file(int ac, char **av)
 {
 	char		*simple_grid;
 	t_params	*params;
 	t_grid		*grid;
 	int		i;
 
-	if (ac == 1)
+	i = 1;
+	while (i < ac)
 	{
-		simple_grid = ft_stdin_to_array();
-		if (!ft_grid_control(simple_grid))
-		{
-			params = new_params(simple_grid);
-			grid = new_grid(params, simple_grid);
-			ft_find_max_square(grid);
-			grid->print(grid);
-		}
+		simple_grid = ft_file_to_array(av[i]);
+		params = new_params(simple_grid);
+		grid = new_grid(params, simple_grid);
+		ft_find_max_square(grid);
+		grid->print(grid);
+		i++;
 	}
-	else
-		multiple_file(ac, av);
-	return (0);
 }
