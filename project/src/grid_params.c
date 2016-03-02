51ef7e3f2de15f6a70b6f2cc47b608d823714616
Kevin Piacentini <kevinpiac@gmail.com>
2016-03-02 21:43:46 +0100

    Fixing issue int params values getters

--- project/src/grid_params.c
+++ project/src/grid_params.c
@@ -6,11 +6,12 @@
 /*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/11 13:11:08 by nhuber            #+#    #+#             */
-/*   Updated: 2016/02/10 17:10:45 by nhuber           ###   ########.fr       */
+/*   Updated: 2016/03/02 21:38:47 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+#include <stdio.h>
 
 char	ft_char_to_fill(char *grid)
 {
@@ -18,6 +19,8 @@ char	ft_char_to_fill(char *grid)
 
 	tab = ft_grid_get_line(grid, 1);
 	tab = ft_strrev(tab);
+	printf("char to fill, tab: %c\n", tab[1]);
+
 	return (tab[1]);
 }
 
@@ -27,6 +30,7 @@ char	ft_char_obstacle(char *grid)
 
 	tab = ft_grid_get_line(grid, 1);
 	tab = ft_strrev(tab);
+	printf("obstacle, tab: %c\n", tab[2]);
 	return (tab[2]);
 }
 
@@ -36,6 +40,7 @@ char	ft_char_empty(char *grid)
 
 	tab = ft_grid_get_line(grid, 1);
 	tab = ft_strrev(tab);
+	printf("char to empty, tab: %c\n", tab[3]);
 	return (tab[3]);
 }
 
@@ -46,12 +51,13 @@ int		ft_number_of_line(char *grid)
 	char	*param_line;
 
 	param_line = ft_grid_get_line(grid, 1);
-	len = ft_strlen(param_line) - 3;
+	len = ft_strlen(param_line) - 4;
 	if (!(tab = (char *)malloc(sizeof(char) * len + 1)))
 		return (0);
 	tab[len] = '\0';
 	tab = ft_strncpy(tab, param_line, len);
 	len = ft_atoi(tab);
+	printf("line nbr, tab: %d\n", len);
 	free(tab);
 	return (len);
 }
